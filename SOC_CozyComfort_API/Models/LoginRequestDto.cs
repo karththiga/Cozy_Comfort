79f2b7ca4d5e5374d57d38b0ede38c7e08504e52
karththiga <karththiga11@gmail.com>
2026-02-10 06:00:46 +0000

    Move login and inventory validations to API layer

---
+++ SOC_CozyComfort_API/Models/LoginRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOC_CozyComfort_API.Models
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
