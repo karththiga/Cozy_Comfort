532f296ff5dbfb11f3868b9b923de4dd3d4da4bb
karththiga <karththiga11@gmail.com>
2026-02-10 07:53:16 +0000

    Add API progress loader for login and CRUD form submissions

---
+++ SOC_Cozy_Comfort_Client/Models/ApiOperationResult.cs
@@ -0,0 +1,8 @@
+namespace SOC_Cozy_Comfort_Client.Models
+{
+    public class ApiOperationResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
