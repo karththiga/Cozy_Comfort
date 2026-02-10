6f8ec6a7a6076a1233cec0fce9c6af74aacac080
karththiga <karththiga11@gmail.com>
2026-02-10 07:22:58 +0000

    Align API/client to SQL Server instance and add CRUD persistence troubleshooting

---
+++ SOC_CozyComfort_API/Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SOC_CozyComfort_API.Services
+{
+    public static class AuthService
+    {
+        private static string ConnectionString => ConfigurationManager.ConnectionStrings["CozyComfortDb"].ConnectionString;
+
+        public static bool IsValidRole(string role)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand("SELECT COUNT(1) FROM dbo.Roles WHERE RoleName = @RoleName", connection))
+            {
+                command.Parameters.AddWithValue("@RoleName", role);
+                connection.Open();
+                return (int)command.ExecuteScalar() > 0;
+            }
+        }
+
+        public static bool IsValidLogin(string userName, string password, string role)
+        {
+            const string sql = @"
+SELECT COUNT(1)
+FROM dbo.Users u
+JOIN dbo.Roles r ON r.Id = u.RoleId
+WHERE u.UserName = @UserName
+  AND u.[Password] = @Password
+  AND r.RoleName = @RoleName";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@UserName", userName);
+                command.Parameters.AddWithValue("@Password", password);
+                command.Parameters.AddWithValue("@RoleName", role);
+                connection.Open();
+                return (int)command.ExecuteScalar() > 0;
+            }
+        }
+    }
+}
---
+++ SOC_Cozy_Comfort_Client/Models/RoleDashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SOC_Cozy_Comfort_Client.Models
+{
+    public class RoleDashboardViewModel
+    {
+        public string Role { get; set; }
+        public string LoggedInUser { get; set; }
+        public List<InventoryItem> Items { get; set; }
+        public InventoryItem NewItem { get; set; }
+    }
+}
