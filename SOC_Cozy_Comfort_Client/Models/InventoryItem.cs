a1c1ced1a33c43b6bdc6fbd88481330e86467ab8
karththiga <karththiga11@gmail.com>
2026-02-10 05:17:57 +0000

    Add inventory CRUD for manufacturer distributor and seller dashboards

---
+++ SOC_Cozy_Comfort_Client/Models/InventoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SOC_Cozy_Comfort_Client.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "SKU")]
+        public string Sku { get; set; }
+
+        [Required]
+        [Display(Name = "Item Name")]
+        public string Name { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        [Display(Name = "Last Updated")]
+        public DateTime LastUpdated { get; set; }
+    }
+}
