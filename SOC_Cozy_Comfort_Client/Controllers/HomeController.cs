7fe9604303e4271056b571b87a09455081a47541
karththiga <karththiga11@gmail.com>
2026-02-10 04:59:35 +0000

    Polish login and dashboards with professional visual design

--- SOC_Cozy_Comfort_Client/Controllers/HomeController.cs
+++ SOC_Cozy_Comfort_Client/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SOC_Cozy_Comfort_Client.Controllers
 {
@@ -13,17 +9,70 @@ namespace SOC_Cozy_Comfort_Client.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string userName, string password, string role)
+        {
+            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(role))
+            {
+                ViewBag.ErrorMessage = "Please enter username, password, and select a role.";
+                return View();
+            }
+
+            TempData["LoggedInUser"] = userName;
+
+            switch (role)
+            {
+                case "Manufacturer":
+                    return RedirectToAction("Manufacturer");
+                case "Distributor":
+                    return RedirectToAction("Distributor");
+                case "Seller":
+                    return RedirectToAction("Seller");
+                default:
+                    ViewBag.ErrorMessage = "Invalid role selected.";
+                    return View();
+            }
+        }
+
+        public ActionResult Manufacturer()
+        {
+            ViewBag.LoggedInUser = TempData["LoggedInUser"];
+            return View();
+        }
+
+        public ActionResult Distributor()
+        {
+            ViewBag.LoggedInUser = TempData["LoggedInUser"];
+            return View();
+        }
+
+        public ActionResult Seller()
+        {
+            ViewBag.LoggedInUser = TempData["LoggedInUser"];
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "SOC architecture overview for Cozy Comfort.";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "Project communication channels.";
             return View();
         }
     }
