d54510bdfbd478debb33a46ecfd7c135002acbd0
karththiga <karththiga11@gmail.com>
2026-02-19 05:44:21 +0000

    Scope seller signup and route distributor data by assignment

--- SOC_CozyComfort_API/Models/AdminCreateUserRequestDto.cs
+++ SOC_CozyComfort_API/Models/AdminCreateUserRequestDto.cs
@@ -1,12 +1,12 @@
 namespace SOC_CozyComfort_API.Models
 {
-    public class SignupRequestDto
+    public class AdminCreateUserRequestDto
     {
+        public string AdminUserName { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Role { get; set; }
         public string Password { get; set; }
-        public int? DistributorUserId { get; set; }
     }
 }
