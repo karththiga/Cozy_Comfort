2ac407a20d5ddb65bb24694ea9b965ffb1ac0cdd
karththiga <karththiga11@gmail.com>
2026-02-20 02:01:19 +0000

    Remove outer signup shell card while keeping main signup card

---
+++ SOC_CozyComfort_API/Models/UserAdminDto.cs
@@ -0,0 +1,12 @@
+namespace SOC_CozyComfort_API.Models
+{
+    public class UserAdminDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
