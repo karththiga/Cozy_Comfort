bc09e86920852f3b3cd30b0c99a0ba1a344838d1
karththiga <karththiga11@gmail.com>
2026-02-20 01:11:18 +0000

    Add admin users table with delete user management

---
+++ SOC_CozyComfort_API/Models/DeleteUserRequestDto.cs
@@ -0,0 +1,8 @@
+namespace SOC_CozyComfort_API.Models
+{
+    public class DeleteUserRequestDto
+    {
+        public string AdminUserName { get; set; }
+        public int UserId { get; set; }
+    }
+}
